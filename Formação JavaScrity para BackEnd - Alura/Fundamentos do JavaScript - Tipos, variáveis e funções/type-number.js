f8ee8f8c78d081aaa8c53609ce3011b975560036
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2021-12-31 14:48:30 -0300

    Estudos da formação da alura sobre JavaScript

---
+++ Formação JavaScrity para BackEnd - Alura/Fundamentos do JavaScript - Tipos, variáveis e funções/type-number.js
@@ -0,0 +1,16 @@
+// Tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaMatematica);
+
+const numeroPontoFlutunate = 3.5;
+const pontoFlutunateSemZero = .5
+
+const novaOperacao = primeiroNumero / numeroPontoFlutunate;
+console.log(novaOperacao);
---
+++ Formação JavaScrity para BackEnd - Alura/Fundamentos do JavaScript - Tipos, variáveis e funções/type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senha123";
+
+const stringDeNumeros = "1234567890";
+
+const aspas = 'O juliano disse "Oi!"';
+const citacao = 'Meu nome é ';
+const meuNome = "Juliano";
+
+console.log(stringDeNumeros);
+
+// Concatenação (+)
+console.log(citacao + meuNome);
+
+// Template string Ou Templete Literal
