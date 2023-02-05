1ca07e50378ceab722a607577ba2a39fbd47a028
juliano-soares <jlsoares@inf.ufsm.br>
2023-02-05 20:22:05 -0300

    Add tipos de variaveis

---
+++ Formação JavaScrity para BackEnd - Alura/Fundamentos do JavaScript - Tipos, variáveis e funções/var-let-const.js
@@ -0,0 +1,15 @@
+// Var
+//var altura = 5
+//var comprimento = 7
+
+//area = altura * altura + comprimento
+//console.log(area)
+//var area
+
+// Let
+let altura = 5
+let comprimento = 7
+area = altura * altura + comprimento
+console.log(area)
+
+// Const
